9471b217d8e3e7f40c7ea8e79f85986d81583421
Cauê <132915276+SantosCaue@users.noreply.github.com>
2023-06-04 16:45:04 -0300

    Offline changes

--- funcao.js
+++ funcao.js
@@ -1,11 +1,19 @@
 function Seleciona() {
     var teste = ["Não se preocupe tanto com o futuro.", "Não deixe que as pessoas te desanimem.", "Não tenha medo de mudar.", "Aproveite o momento presente.", "Não se esqueça de cuidar de si mesmo.", "Não se preocupe tanto com os detalhes.", "Não tenha medo de ser você mesmo.", "Não tenha medo de se abrir para novas possibilidades.", "Aproveite a vida ao máximo.", "Não deixe que o trabalho consuma sua vida.", "Não tenha medo de ser diferente.", "Acredite em si mesmo e siga em frente."];
     let gabsn = 0;
-    for(let i = 0; i < 12; i++){
-        document.getElementsByClassName("answers")[i].innerHTML = "";
+    
+        for(let i = 0; i < document.getElementsByClassName("ativo").length; i++){
+        document.getElementsByClassName("ativo")[i].innerHTML = "";
+        document.getElementsByClassName("ativo")[i].classList.replace('ativo', 'answers');
     }
     while (document.signo.questao1[gabsn].checked == false) {
         gabsn++;
-    }
-    document.getElementsByClassName("answers")[gabsn].innerHTML = teste[gabsn];
+ }
+document.getElementsByClassName("answers")[gabsn].innerHTML = teste[gabsn];
+document.getElementsByClassName("answers")[gabsn].classList.replace('answers', 'ativo');
+let margem = 47.2*(gabsn);
+margem = margem +"px 0px 0px 0px";
+alert(margem)
+document.getElementsByClassName("ativo")[0].style.margin = margem
+
 }
