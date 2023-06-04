a40ee5d66b7f8d09fbea8837854601a9af9f5431
SantosCaue <132915276+SantosCaue@users.noreply.github.com>
2023-06-04 10:16:15 -0300

    Tentativa de correção na compactação do js

--- funcao2.js
+++ funcao2.js
@@ -1,13 +1,15 @@
 function Seleciona() {
-    var teste = ("Não se preocupe tanto com o futuro.", "Não deixe que as pessoas te desanimem.", "Não tenha medo de mudar.", "Aproveite o momento presente.", "Não se esqueça de cuidar de si mesmo.", "Não se preocupe tanto com os detalhes.", "Não tenha medo de ser você mesmo.", "Não tenha medo de se abrir para novas possibilidades.", "Aproveite a vida ao máximo.", "Não deixe que o trabalho consuma sua vida.", "Não tenha medo de ser diferente.", "Acredite em si mesmo e siga em frente.");
+    var teste = ["Não se preocupe tanto com o futuro.", "Não deixe que as pessoas te desanimem.", "Não tenha medo de mudar.", "Aproveite o momento presente.", "Não se esqueça de cuidar de si mesmo.", "Não se preocupe tanto com os detalhes.", "Não tenha medo de ser você mesmo.", "Não tenha medo de se abrir para novas possibilidades.", "Aproveite a vida ao máximo.", "Não deixe que o trabalho consuma sua vida.", "Não tenha medo de ser diferente.", "Acredite em si mesmo e siga em frente."];
     let gabsn = 0;
+    var botao = document.getElementsByClassName("answers");
     for(let i; i<12; i++){
-        var botao = answers[i];
-        document.getElementsByClassName(botao).innerHTML="";
+        var klau = botao[i];
+        document.klau.innerHTML=""
     }
     while (document.signo.questao1[gabsn].checked == false) {
         gabsn++;
     }
-    document.getElementById(answers[gabsn]).innerHTML=teste[gabsn];
+    klau = botao[gabsn];
+    document.klau.innerHTML=teste[gabsn];
 
 }
