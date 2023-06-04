0ad37e6a4175cab9a9ffcebc39cfcb5f9c060321
SantosCaue <132915276+SantosCaue@users.noreply.github.com>
2023-06-04 09:39:26 -0300

    Compactação do JS

---
+++ funcao2.js
@@ -0,0 +1,13 @@
+function Seleciona() {
+    var teste = ("Não se preocupe tanto com o futuro.", "Não deixe que as pessoas te desanimem.", "Não tenha medo de mudar.", "Aproveite o momento presente.", "Não se esqueça de cuidar de si mesmo.", "Não se preocupe tanto com os detalhes.", "Não tenha medo de ser você mesmo.", "Não tenha medo de se abrir para novas possibilidades.", "Aproveite a vida ao máximo.", "Não deixe que o trabalho consuma sua vida.", "Não tenha medo de ser diferente.", "Acredite em si mesmo e siga em frente.");
+    let gabsn = 0;
+    for(let i; i<12; i++){
+        var botao = answer[i];
+        document.getElementsByClassName(botao).innerHTML="";
+    }
+    while (document.signo.questao1[gabsn].checked == false) {
+        gabsn++;
+    }
+    document.getElementById(answer[gabsn]).innerHTML=teste[gabsn];
+
+}
